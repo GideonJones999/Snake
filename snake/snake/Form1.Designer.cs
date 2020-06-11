e931bd271e370a4badd5d4a5d1f94dc1d90ac7ba
Gideon Jones <gideon.w.jones@gmail.com>
2020-06-11 11:12:38 -0600

    Init Commit

---
+++ snake/snake/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Score = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.food = new System.Windows.Forms.PictureBox();
+            this.gameTime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Score.Location = new System.Drawing.Point(925, 35);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(86, 20);
+            this.Score.TabIndex = 0;
+            this.Score.Text = "Score:   1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::snake.Properties.Resources.Instructions;
+            this.pictureBox1.Location = new System.Drawing.Point(930, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Controls.Add(this.food);
+            this.panel1.Location = new System.Drawing.Point(15, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 600);
+            this.panel1.TabIndex = 2;
+            // 
+            // food
+            // 
+            this.food.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.food.Image = global::snake.Properties.Resources.kitten;
+            this.food.Location = new System.Drawing.Point(600, 300);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(30, 30);
+            this.food.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.food.TabIndex = 0;
+            this.food.TabStop = false;
+            // 
+            // gameTime
+            // 
+            this.gameTime.Enabled = true;
+            this.gameTime.Interval = 200;
+            this.gameTime.Tick += new System.EventHandler(this.gameTime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(1184, 631);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Score);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Snake Byte !!!";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.food)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox food;
+        private System.Windows.Forms.Timer gameTime;
+    }
+}
+
---
+++ snake/snake/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        List<PictureBox> Snake = new List<PictureBox>();
+        Random numb = new Random();
+        int ySpeed = 30, xSpeed = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SnakeBody();
+            Snake[0].Image = Properties.Resources.down;
+
+        }
+
+        private void SnakeBody()
+        {
+            PictureBox temp = new PictureBox();
+            temp.Image = Properties.Resources.body;
+            temp.SizeMode = PictureBoxSizeMode.StretchImage;
+            temp.BackColor = Color.Transparent;
+            temp.Width = temp.Height = 28;
+            Snake.Add(temp);
+            panel1.Controls.Add(temp);
+        }
+
+        private void gameTime_Tick(object sender, EventArgs e)
+        {
+            //Eat
+            if (Snake[0].Bounds.IntersectsWith(food.Bounds))
+                Eat();
+
+            //Move Body
+            for (int i = Snake.Count - 1; i > 0; i--)
+            {
+                Snake[i].Left = Snake[i - 1].Left;
+                Snake[i].Top = Snake[i - 1].Top;
+            }
+            //Move Head
+            Snake[0].Top += ySpeed;
+            Snake[0].Left += xSpeed;
+
+            //Fall off world
+            if (!Snake[0].Bounds.IntersectsWith(panel1.Bounds))
+                DEATH();
+
+            //Cross Body
+            for (int i = Snake.Count-1; i > 0; i--)
+            {
+                if (Snake[0].Bounds.IntersectsWith(Snake[i].Bounds))
+                    DEATH();
+            }
+
+        }
+
+        private void DEATH()
+        {
+            Score.Text += "\nPress ''Space'' to Play Again!";
+            gameTime.Stop();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    xSpeed = 0;
+                    ySpeed = -30;
+                    Snake[0].Image = Properties.Resources.up;
+                    break;
+                case Keys.W:
+                    xSpeed = 0;
+                    ySpeed = -30;
+                    Snake[0].Image = Properties.Resources.up;
+                    break;
+                case Keys.Left:
+                    xSpeed = -30;
+                    ySpeed = 0;
+                    Snake[0].Image = Properties.Resources.left;
+                    break;
+                case Keys.A:
+                    xSpeed = -30;
+                    ySpeed = 0;
+                    Snake[0].Image = Properties.Resources.left;
+                    break;
+                case Keys.Right:
+                    xSpeed = 30;
+                    ySpeed = 0;
+                    Snake[0].Image = Properties.Resources.right;
+                    break;
+                case Keys.D:
+                    xSpeed = 30;
+                    ySpeed = 0;
+                    Snake[0].Image = Properties.Resources.right;
+                    break;
+                case Keys.Down:
+                    xSpeed = 0;
+                    ySpeed = 30;
+                    Snake[0].Image = Properties.Resources.down;
+                    break;
+                case Keys.S:
+                    xSpeed = 0;
+                    ySpeed = 30;
+                    Snake[0].Image = Properties.Resources.down;
+                    break;
+                case Keys.Escape:
+                    this.Close();
+                    break;
+                case Keys.Space:
+                    Application.Restart();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Eat()
+        {
+            // Eat Kitten
+            SnakeBody();
+
+            //Move Kitten
+            food.Left = numb.Next(30) * 30;
+            food.Top = numb.Next(20) * 30;
+
+            //Update Score
+            Score.Text = "Score:   "+ Snake.Count;
+        }
+    }
+}
